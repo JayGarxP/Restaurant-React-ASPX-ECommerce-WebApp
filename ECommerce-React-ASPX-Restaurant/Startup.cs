99cd3f79f5236c25a1c1586821939bc9d1c1e536
JP Fallon <JayGarxP@users.noreply.github.com>
2019-09-18 10:51:50 -0700

    Add project files.

---
+++ ECommerce-React-ASPX-Restaurant/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ECommerce_React_ASPX_Restaurant.Startup))]
+namespace ECommerce_React_ASPX_Restaurant
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
