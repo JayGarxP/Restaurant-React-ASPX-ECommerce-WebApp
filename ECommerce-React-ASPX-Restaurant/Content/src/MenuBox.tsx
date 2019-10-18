1b666e08ac952d924d8e8e34fc82c7644337c096
JP Fallon <JayGarxP@users.noreply.github.com>
2019-10-17 19:40:03 -0700

    shopping cart wireframe react, halfway done, forgot about checkout as guest

--- ECommerce-React-ASPX-Restaurant/Content/src/MenuBox.tsx
+++ ECommerce-React-ASPX-Restaurant/Content/src/MenuBox.tsx
@@ -14,7 +14,23 @@ export class MenuBox extends React.Component<any, IAppState>
     {
         super(state);
         this.state = { items: null, myOrder: null, showPopup: false, userId: 0, orderPlaced: false };
+        this.getLoginStatus();
         this.loadMenusFromServer();
+    }
+
+    getLoginStatus()
+    {
+        var xhr = new XMLHttpRequest();
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', '/data/GetUserId/', true);
+        xhr.onload = function () {
+            var userid: number = parseInt(xhr.responseText);
+            var tmp: IAppState = this.state;
+            tmp.userId = userid;
+            this.setState(tmp);
+
+        }.bind(this);
+        xhr.send();
     }
 
     // function to retrieve menu from server data; used to populate list of food items in menubox ctor
@@ -31,6 +47,37 @@ export class MenuBox extends React.Component<any, IAppState>
         xhr.send();
     }
 
+    addToCart(id)
+    {
+        if (this.state.userId < 1) {
+            // TODO: checkout as guest functionality
+            alert("Please Log in to Add to Cart");
+            return;
+
+        }
+        // reduce by one to map id from database, 1 based in db to collection 0 based index
+        id--;
+        var myCart = this.state.myOrder || []; // empty array if state.myOrder is null
+
+        var allItems = this.state.items;
+        if (myCart.indexOf(allItems[id]) > -1) {
+            var itemToOrder = myCart.find(m => m.Id === allItems[id].Id);
+            itemToOrder["Quantity"] = itemToOrder["Quantity"] + 1;
+        }
+        else {
+            var itemToOrder = allItems[id];
+            itemToOrder["Quantity"] = 1;
+            myCart.push(allItems[id]);
+        }
+
+        var tmp: IAppState = this.state;
+        tmp.myOrder = myCart;
+        tmp.showPopup = false;
+        this.setState(tmp); // prop does not exist in IDE intelliS ???
+
+
+    }
+
     render()
     {
         let menus = this.state.items || [];
@@ -39,7 +86,8 @@ export class MenuBox extends React.Component<any, IAppState>
                 <div key={menu.Id}>
                     <b>{menu.Name} </b>    <br />
                     <img style={{ width: '100px', float: 'left', margin: '5px' }} src={"/img/" + menu.Picture} />{menu.Description}<p />
-                    <div>${menu.Price}</div><hr />
+                    <div>${menu.Price} | <a href='#' onClick={this.addToCart.bind(this, menu.Id)}
+                    >Add to Cart</a></div><hr />
                 </div>
                 )
         }, this);
